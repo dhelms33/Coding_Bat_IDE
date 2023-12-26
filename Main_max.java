74a9fe260141f8ee9b72cb31aeb5fb70a83ebda5
dhelms33 <derrickhelms10@gmail.com>
2023-12-25 20:10:18 -0700

    Added Main and Solution Classses

---
+++ Main_max.java
@@ -0,0 +1,11 @@
+/**
+ * The Main class is the entry point of the program.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution test1 = new Solution();
+        int result = test1.intMax(1, 2, 3);
+        System.out.println("Maxmimum Integer: " + result);
+    }
+
+}
